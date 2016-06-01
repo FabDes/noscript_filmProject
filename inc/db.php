b6e2ab0dd7e2b495c4c02db6c4329f63d84e33a1
marierolland <marie.rolland@gmail.com>
2016-06-01 15:32:43 +0200

    2

---
+++ inc/db.php
@@ -0,0 +1,6 @@
+<?php
+
+
+//je me connecte à la DB
+$dsn = 'mysql:host=192.168.210.84;dbname=marie_sql1;charset=utf8';
+$pdo = new PDO($dsn, 'marie', 'marie');
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// je factorise et je cree PDO et j inclus DB
+require 'inc/db.php';
+
+
+$categorySort= array();
+//j ecris ma requete pour la recherche
+$sql='
+SELECT COUNT(mov_id), category.cat_name
+FROM movie
+INNER JOIN category ON category.cat_id = movie.cat_id
+GROUP BY cat_name
+LIMIT 4 ASC
+';
+$pdoStatement = $pdo->prepare($sql);
+
+if ($pdoStatement ->execute() === false){
+	print_r($pdo->errorInfo());
+}
+else if ($pdoStatement->rowCount()>0){
+	$categorySort= $pdoStatement->fetchAll();
+	print_r($categorySort);
+}
